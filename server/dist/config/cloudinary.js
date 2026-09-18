2a2aa5057e4e6e80a5d43741833de9b7767948e6
PallaviJanjal <pallavijanjal30@gmail.com>
2026-09-18 20:01:31 +0530

    Allow navigating to equipment detail page by clicking equipment image

--- server/dist/config/cloudinary.js
+++ server/dist/config/cloudinary.js
@@ -7,9 +7,9 @@ const cloudinary_1 = require("cloudinary");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 cloudinary_1.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME || 'krushiconnect',
-    api_key: process.env.CLOUDINARY_API_KEY || process.env.API_KEY || '',
-    api_secret: process.env.CLOUDINARY_API_SECRET || process.env.API_SECRET || '',
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 });
 exports.default = cloudinary_1.v2;
